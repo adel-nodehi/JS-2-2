87868906b6f059590dadbbb91fee31342fd48588
Adel <adelnmo2003@gmail.com>
2024-09-14 22:38:51 +0330

    object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+"use strict";
+
+/*
 const calcAge2 = function (birthYear) {
   return 2024 - birthYear;
 };
@@ -9,3 +12,33 @@ const displayInfo = function (name, birthYear) {
 };
 
 console.log(displayInfo("Saeed", 2006));
+*/
+/*
+const displayInfo = function (person) {
+  console.log(
+    `My name is ${person.name} and i'm ${person.age} years old
+     and my job is ${person.job}.`
+  );
+};
+
+const narges = {
+  name: "Narges",
+  age: 18,
+  job: "developer",
+
+  showInfo: function () {
+    console.log(
+      `My name is ${this.name} and i'm ${this.age} years old
+        and my job is ${this.job}.`
+    );
+  },
+};
+// narges.showInfo();
+
+displayInfo(narges);
+
+// maryam.sports = ["football", "taekwondo"];
+// maryam["spor" + "ts"] = ["football", "taekwondo"];
+
+// console.log([2, 3].includes());
+*/
